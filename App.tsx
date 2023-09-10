d8cad16617d4c30777efdd486d2d5a4466fc2857
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2023-09-09 22:21:58 -0300

    add dependencies and basic configuration for react navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Text>SaveMyWalletApp</Text>
+    </NavigationContainer>
   );
 };
 
